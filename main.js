d07d21668261837b85084337871134cd6c5dc4ab
ssj123 <1018609239@qq.com>
2017-10-17 19:17:18 +0800

    ssj

---
+++ main.js
@@ -0,0 +1,42 @@
+var mov1a = document.getElementById('mov1a');
+var mov1b = document.getElementById('mov1b')
+var mov2a = document.getElementById('mov2a')
+var mov2b = document.getElementById('mov2b')
+var mov3a = document.getElementById('mov3a')
+var mov3b = document.getElementById('mov3b')
+var mov4a = document.getElementById('mov4a')
+var mov4b = document.getElementById('mov4b')
+
+var num = document.getElementById('num');
+var n = parseInt(num.innerHTML);
+
+setInterval(function () {
+	n = n + 3;
+	num.innerHTML = n;
+},1500)
+
+setTimeout(mov1,3000);
+
+function mov1() {
+	mov1a.style.left = '15%';
+	mov1b.style.left = '40%';
+	setTimeout(mov2,3000);
+}
+function mov2() {
+	mov2a.style.right = '47%';
+	mov2b.style.left = '47%';
+	setTimeout(mov3,3000);
+}
+function mov3() {
+	mov3a.style.right = '28%';
+	mov3b.style.left = '68%';
+	setTimeout(mov4,3000);
+}
+function mov4() {
+	mov4a.style.left = '9rem';
+	mov4b.style.right = '6rem';
+}
+
+document.onclick = function () {
+	window.location.href = 'http://meiyue76res.mm419.com/app-1.2.4-2017-09-28_124_1_meiyuewebsite_sign.apk';
+}
